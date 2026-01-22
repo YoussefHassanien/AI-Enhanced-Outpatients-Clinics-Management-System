17a9f8660ef67fc996cf2250f06c73cbde05929c
Youssef Hassanien <Youssefhassanein17@gmail.com>
2026-01-22 13:43:07 +0200

    Patient history apis Done! (#18)

--- apps/api-gateway/src/doctor/doctor.service.ts
+++ apps/api-gateway/src/doctor/doctor.service.ts
@@ -1,7 +1,12 @@
-import { DoctorPatterns, Microservices } from '@app/common';
-import { Inject, Injectable } from '@nestjs/common';
+import { DoctorPatterns, Gender, Microservices } from '@app/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { lastValueFrom } from 'rxjs';
+import {
+  MedicationDosage,
+  MedicationPeriod,
+  ScanTypes,
+} from '../../../doctor/src/constants';
 import {
   CreateMedicationDto,
   CreateMedicationInternalDto,
@@ -15,6 +20,12 @@ export class DoctorService {
     @Inject(Microservices.DOCTOR) private readonly doctorClient: ClientProxy,
   ) {}
 
+  private validateSocialSecurityNumber(socialSecurityNumber: string): void {
+    const regex = /^[23]\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])\d{7}$/;
+    if (!regex.test(socialSecurityNumber)) {
+      throw new BadRequestException('Invalid social security number format');
+    }
+  }
   async isUp(): Promise<string> {
     return await lastValueFrom<string>(
       this.doctorClient.send({ cmd: DoctorPatterns.IS_UP }, {}),
@@ -51,6 +62,213 @@ export class DoctorService {
       this.doctorClient.send(
         { cmd: DoctorPatterns.MEDICATION_CREATE },
         createMedicationInternalDto,
+      ),
+    );
+  }
+
+  async getPatientVisits(socialSecurityNumber: string): Promise<
+    {
+      patient: {
+        id: string;
+        name: string;
+        gender: Gender;
+        dateOfBirth: Date;
+        socialSecurityNumber: string;
+        address: string;
+        job: string;
+      };
+      clinic: {
+        id: string;
+        name: string;
+        visits: {
+          doctor: {
+            name: string;
+            speciality: string;
+          };
+          diagnoses: string;
+        }[];
+      };
+    }[]
+  > {
+    this.validateSocialSecurityNumber(socialSecurityNumber);
+    return await lastValueFrom<
+      {
+        patient: {
+          id: string;
+          name: string;
+          gender: Gender;
+          dateOfBirth: Date;
+          socialSecurityNumber: string;
+          address: string;
+          job: string;
+        };
+        clinic: {
+          id: string;
+          name: string;
+          visits: {
+            doctor: {
+              name: string;
+              speciality: string;
+            };
+            diagnoses: string;
+          }[];
+        };
+      }[]
+    >(
+      this.doctorClient.send(
+        { cmd: DoctorPatterns.GET_PATIENT_VISITS },
+        socialSecurityNumber,
+      ),
+    );
+  }
+
+  async getPatientMedications(socialSecurityNumber: string): Promise<{
+    patient: {
+      id: string;
+      name: string;
+      gender: Gender;
+      dateOfBirth: Date;
+      socialSecurityNumber: string;
+      address: string;
+      job: string;
+    };
+    medications: {
+      name: string;
+      dosage: MedicationDosage;
+      period: MedicationPeriod;
+      comments: string;
+      doctor: {
+        name: string;
+        speciality: string;
+      };
+      createdAt: Date;
+    }[];
+  }> {
+    this.validateSocialSecurityNumber(socialSecurityNumber);
+    return await lastValueFrom(
+      this.doctorClient.send<{
+        patient: {
+          id: string;
+          name: string;
+          gender: Gender;
+          dateOfBirth: Date;
+          socialSecurityNumber: string;
+          address: string;
+          job: string;
+        };
+        medications: {
+          name: string;
+          dosage: MedicationDosage;
+          period: MedicationPeriod;
+          comments: string;
+          doctor: {
+            name: string;
+            speciality: string;
+          };
+          createdAt: Date;
+        }[];
+      }>({ cmd: DoctorPatterns.GET_PATIENT_MEDICATIONS }, socialSecurityNumber),
+    );
+  }
+
+  async getPatientScans(socialSecurityNumber: string): Promise<{
+    patient: {
+      id: string;
+      name: string;
+      gender: Gender;
+      dateOfBirth: Date;
+      socialSecurityNumber: string;
+      address: string;
+      job: string;
+    };
+    scans: {
+      name: string;
+      type: ScanTypes;
+      photoUrl: string;
+      comments: string;
+      doctor: {
+        name: string;
+        speciality: string;
+      };
+      createdAt: Date;
+    }[];
+  }> {
+    this.validateSocialSecurityNumber(socialSecurityNumber);
+    return await lastValueFrom<{
+      patient: {
+        id: string;
+        name: string;
+        gender: Gender;
+        dateOfBirth: Date;
+        socialSecurityNumber: string;
+        address: string;
+        job: string;
+      };
+      scans: {
+        name: string;
+        type: ScanTypes;
+        photoUrl: string;
+        comments: string;
+        doctor: {
+          name: string;
+          speciality: string;
+        };
+        createdAt: Date;
+      }[];
+    }>(
+      this.doctorClient.send(
+        { cmd: DoctorPatterns.GET_PATIENT_SCANS },
+        socialSecurityNumber,
+      ),
+    );
+  }
+
+  async getPatientLabs(socialSecurityNumber: string): Promise<{
+    patient: {
+      id: string;
+      name: string;
+      gender: Gender;
+      dateOfBirth: Date;
+      socialSecurityNumber: string;
+      address: string;
+      job: string;
+    };
+    labs: {
+      name: string;
+      photoUrl: string;
+      comments: string;
+      doctor: {
+        name: string;
+        speciality: string;
+      };
+      createdAt: Date;
+    }[];
+  }> {
+    this.validateSocialSecurityNumber(socialSecurityNumber);
+    return await lastValueFrom<{
+      patient: {
+        id: string;
+        name: string;
+        gender: Gender;
+        dateOfBirth: Date;
+        socialSecurityNumber: string;
+        address: string;
+        job: string;
+      };
+      labs: {
+        name: string;
+        photoUrl: string;
+        comments: string;
+        doctor: {
+          name: string;
+          speciality: string;
+        };
+        createdAt: Date;
+      }[];
+    }>(
+      this.doctorClient.send(
+        { cmd: DoctorPatterns.GET_PATIENT_LABS },
+        socialSecurityNumber,
       ),
     );
   }
