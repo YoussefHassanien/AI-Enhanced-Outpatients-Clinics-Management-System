4db44d40540ae9601b39641b6555b7413739a222
Zeyad Khaled <z.badry19@gmail.com>
2026-01-24 20:40:04 +0200

    feat(admin): expose get patient and doctor by id endpoints with response DTOs

---
+++ apps/admin/src/dtos/doctor-response.dto.ts
@@ -0,0 +1,14 @@
+import { Gender } from '@app/common';
+
+export class DoctorResponseDTO {
+  id: string;
+  phone: string;
+  email: string;
+  speciality: string;
+  isApproved: boolean;
+  socialSecurityNumber: bigint;
+  gender: Gender;
+  firstName: string;
+  lastName: string;
+  dateOfBirth: Date;
+}
---
+++ apps/admin/src/dtos/patient-response.dto.ts
@@ -0,0 +1,12 @@
+import { Gender } from '@app/common';
+
+export class PatientResponseDTO {
+  id: string;
+  address: string;
+  job: string;
+  socialSecurityNumber: bigint;
+  gender: Gender;
+  firstName: string;
+  lastName: string;
+  dateOfBirth: Date;
+}
