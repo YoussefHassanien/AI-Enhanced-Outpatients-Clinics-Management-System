b23475ae34b60ccf3189c42bbb40d974284cfb6f
AbdelRahman Hesham <115306247+H3SHAM03@users.noreply.github.com>
2026-01-25 17:38:45 +0200

    pulling new doctor endpoints (#26)
    * Doctor service initial setup
    
    * Added patient, doctor, and admin get patterns in auth service for internal communication
    
    * Doctor module integration with api-gateway Done!
    
    * Medication creation Done!, Overall database and performance enhancement
    
    * Fix: Docker compose duplicated doctor service
    
    * Fix: Create doctor api problem solved
    
    * Patient history apis Done!
    
    * getDoctorPatients and getDoctorVisits endpoints done
    
    * tiny fix
    
    * small tweak for responses to match the requested ones
    
    ---------
    
    Co-authored-by: Youssef Hassanien <Youssefhassanein17@gmail.com>

--- apps/doctor/src/dtos/index.ts
+++ apps/doctor/src/dtos/index.ts
@@ -2,3 +2,5 @@ export * from './create-medication-internal.dto';
 export * from './create-medication.dto';
 export * from './create-visit-internal.dto';
 export * from './create-visit.dto';
+export * from './get-doctor-patients.dto';
+export * from './get-doctor-visits.dto';
