0f64ed65459338fc0803a12096dfd5cdf95d2e81
Zeyad Khaled <z.badry19@gmail.com>
2026-04-13 21:39:12 +0200

    Modifying the way the ASR application works to be hosted entirely on our server

--- apps/asr/src/dtos/transcribe-audio-Internal.dto.ts
+++ apps/asr/src/dtos/transcribe-audio-Internal.dto.ts
@@ -1,7 +1,12 @@
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 import { PathLike } from 'fs';
 
 export class TranscribeAudioInternalDto {
+  @IsOptional()
   @IsString()
-  filePath: PathLike;
+  filePath?: PathLike;
+
+  @IsOptional()
+  @IsString()
+  audio_base64?: string;
 }
